1391f8d610c271c536dfc86a9181d7c04a3cd1b6
worroc <worroc@zoho.com>
2024-07-03 08:48:49 +0300

    Bump version: 0.0.2 → 0.0.3

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(LICENSE) as f:
 
 setup(
     name="lockeye",
-    version="0.0.2",
+    version="0.0.3",
     description="code monitor",
     long_description=readme,
     author="Worroc",
